8e6e0ff211204829745c2da9dffbbf9f1695bea4
pphatdev <leatsophat02@gmail.com>
2025-04-01 01:39:56 +0700

    Add Logos3 component to HeroSection for displaying trusted company logos

--- src/app/(home)/sections/hero.tsx
+++ src/app/(home)/sections/hero.tsx
@@ -11,8 +11,56 @@ import { ThemeToggle } from "@components/ui/theme-switch";
 import { MagneticArea } from "@components/ui/magnetic-button";
 import { cn } from "@lib/utils";
 import Link from "next/link";
+import { Logos3 } from "@components/ui/logos3";
 // import { Button } from '../../../components/ui/moving-border';
 
+const demoData = {
+    heading: "Trusted by these companies",
+    logos: [
+        {
+            id: "logo-1",
+            description: "Astro",
+            image: "https://www.shadcnblocks.com/images/block/logos/astro.svg",
+            className: "h-7 w-auto",
+        },
+        {
+            id: "logo-2",
+            description: "Figma",
+            image: "https://www.shadcnblocks.com/images/block/logos/figma.svg",
+            className: "h-7 w-auto",
+        },
+        {
+            id: "logo-3",
+            description: "Next.js",
+            image: "https://www.shadcnblocks.com/images/block/logos/nextjs.svg",
+            className: "h-7 w-auto",
+        },
+        {
+            id: "logo-4",
+            description: "React",
+            image: "https://www.shadcnblocks.com/images/block/logos/react.png",
+            className: "h-7 w-auto",
+        },
+        {
+            id: "logo-6",
+            description: "Supabase",
+            image: "https://www.shadcnblocks.com/images/block/logos/supabase.svg",
+            className: "h-7 w-auto",
+        },
+        {
+            id: "logo-7",
+            description: "Tailwind CSS",
+            image: "https://www.shadcnblocks.com/images/block/logos/tailwind.svg",
+            className: "h-4 w-auto",
+        },
+        {
+            id: "logo-8",
+            description: "Vercel",
+            image: "https://www.shadcnblocks.com/images/block/logos/vercel.svg",
+            className: "h-7 w-auto",
+        },
+    ],
+};
 
 
 export default function HeroSection() {
@@ -52,22 +100,24 @@ export default function HeroSection() {
                     <TextAnimate animation="slideLeft" by="word" className="whitespace-pre-wrap mt-5">
                         {appDescriptions ?? ""}
                     </TextAnimate>
-                    <nav className="flex flex-col z-50 justify-center sm:flex-row items-center gap-4 mt-6">
+                    <Logos3 {...demoData} />
+                    <nav className="flex flex-col z-50 justify-center sm:justify-start sm:flex-row items-center gap-4 mt-6">
                         <MagneticArea>
                             <ThemeToggle />
                         </MagneticArea>
                     </nav>
-                    <NavMenu />
+                    {/* <NavMenu /> */}
+
                 </div>
                 <div className={cn("order-first relative mt-10 sm:mt-0 shrink-0 md:order-last")}>
                     <MagneticArea >
                         <div className={cn(
-                            "absolute -z-[1] w-full h-full blur-3xl  left-1/2 -bottom-1/3 -translate-x-1/2 opacity-20 animate-rainbow",
+                            "absolute -z-[1] w-full h-full blur-3xl left-1/2 translate-y-1/2 bottom-1/3 -translate-x-1/2 opacity-20 animate-rainbow",
                             "bg-[linear-gradient(90deg,hsl(var(--color-1)),hsl(var(--color-5)),hsl(var(--color-3)),hsl(var(--color-4)),hsl(var(--color-2)))]",
                             "bg-[length:200%]",
                         )}></div>
 
-                        <Link href={'/gallery'}>
+                        <Link href={'/gallery'} className="flex mx-auto items-center justify-center overflow-hidden w-36 h-36 sm:w-80 sm:h-80 rounded-full bg-gradient-to-r from-sky-500/5 via-teal-500/5 to-green-500/5 hover:scale-125 transition-transform duration-300 ease-in-out">
                             <Image
                                 src="/assets/avatars/hero.webp"
                                 width={512}
@@ -82,6 +132,7 @@ export default function HeroSection() {
                         </Link>
 
                     </MagneticArea>
+                    <NavMenu />
                 </div>
             </section>
         </main >
