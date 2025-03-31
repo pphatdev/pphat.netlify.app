a7c91f8c210c06922fed3f10a353f1bfa1a8be03
PPhat DEv <pphat.kits@gmail.com>
2025-03-31 17:28:26 +0700

    Add TestDb component for testing database creation functionality

---
+++ src/app/faq/test.tsx
@@ -0,0 +1,11 @@
+"use client";
+// import { createDB } from "@lib/db/db";
+
+export const TestDb = () => {
+
+    // createDB("test", { timestamps: true, version: 1, type: "indexedDB" })
+
+    // console.log(createDB("test", { timestamps: true, version: 1, type: "indexedDB" }));
+
+    return <></>
+}
