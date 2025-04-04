df44111d9a417c70690ccadfe629ab00e7373881
pphatdev <leatsophat02@gmail.com>
2025-04-05 00:59:45 +0700

    Refactor ProgressBarProvider to include navigation items and enhance layout with FloatingNav

--- src/components/ProgressBarProvider.tsx
+++ src/components/ProgressBarProvider.tsx
@@ -1,10 +1,34 @@
 'use client';
 
 import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
+import { FloatingNav } from './ui/floating-navbar';
+import { IconHome, IconMessage, IconUser } from '@tabler/icons-react';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
+
+    const navItems = [
+        {
+            name: "Home",
+            link: "/",
+            icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+        },
+        {
+            name: "About",
+            link: "/?",
+            icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+        },
+        {
+            name: "Contact",
+            link: "/?",
+            icon: (
+                <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+            ),
+        },
+    ];
+
     return (
         <>
+            <FloatingNav navItems={navItems} />
             {children}
             <ProgressBar
                 height="4px"
