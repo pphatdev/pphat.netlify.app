925f2ad1da437f6f1da5ca3f859432156d9aeae8
pphatdev <leatsophat02@gmail.com>
2025-04-07 08:15:14 +0700

    Refactor InfiniteScroll component to use named imports for React and improve code consistency

--- src/components/infinit-scroll.tsx
+++ src/components/infinit-scroll.tsx
@@ -1,4 +1,5 @@
-import React, { cloneElement, isValidElement, useCallback, useMemo, useRef } from 'react';
+import * as React from 'react';
+
 interface InfiniteScrollProps {
     isLoading: boolean;
     hasMore: boolean;
@@ -20,14 +21,16 @@ export default function InfiniteScroll({
     reverse,
     children,
 }: InfiniteScrollProps) {
-    const observer = useRef<IntersectionObserver>(null);
+    const observer = React.useRef<IntersectionObserver>(null);
     // This callback ref will be called when it is dispatched to an element or detached from an element,
     // or when the callback function changes.
-    const observerRef = useCallback(
+    const observerRef = React.useCallback(
         (element: HTMLElement | null) => {
             let safeThreshold = threshold;
             if (threshold < 0 || threshold > 1) {
-                console.warn('threshold should be between 0 and 1. You are exceed the range. will use default value: 1',);
+                console.warn(
+                    'threshold should be between 0 and 1. You are exceed the range. will use default value: 1',
+                );
                 safeThreshold = 1;
             }
             // When isLoading is true, this callback will do nothing.
@@ -52,12 +55,12 @@ export default function InfiniteScroll({
         [hasMore, isLoading, next, threshold, root, rootMargin],
     );
 
-    const flattenChildren = useMemo(() => React.Children.toArray(children), [children]);
+    const flattenChildren = React.useMemo(() => React.Children.toArray(children), [children]);
 
     return (
         <>
             {flattenChildren.map((child, index) => {
-                if (!isValidElement(child)) {
+                if (!React.isValidElement(child)) {
                     process.env.NODE_ENV === 'development' &&
                         console.warn('You should use a valid element with InfiniteScroll');
                     return child;
@@ -66,7 +69,7 @@ export default function InfiniteScroll({
                 const isObserveTarget = reverse ? index === 0 : index === flattenChildren.length - 1;
                 const ref = isObserveTarget ? observerRef : null;
                 // @ts-ignore ignore ref type
-                return cloneElement(child, { ref });
+                return React.cloneElement(child, { ref });
             })}
         </>
     );
