6a67646a294cdaf40ddf567e16ef6a5b79ad1833
PPhat DEv <pphat.kits@gmail.com>
2025-04-10 16:20:15 +0700

    feat: add ProjectHero component for project section layout

---
+++ src/app/projects/hero.tsx
@@ -0,0 +1,10 @@
+export const ProjectHero = () => {
+    return (
+        <section className="min-h-96 flex flex-col border-b items-start justify-center">
+            <div className="w-full p-4 sm:px-10 max-w-5xl mx-auto ">
+
+                <h1 className='mb-4 text-5xl font-bold'>Projects</h1>
+            </div>
+        </section>
+    )
+}
