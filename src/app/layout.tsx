a9d30f847bbc4aefd7fef414ad737e122baf1af1
pphatdev <leatsophat02@gmail.com>
2025-04-15 21:09:56 +0700

    fix: add missing React import in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import React from "react";
 import type { Metadata } from "next";
 import Providers from "../components/ProgressBarProvider";
 import { ThemeProvider } from 'next-themes'
