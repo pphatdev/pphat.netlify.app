7a3a0acae8c1dced523e47e3b2405a3dcadfa1d9
pphatdev <leatsophat02@gmail.com>
2025-04-16 18:20:38 +0700

    feat: create LoginPage component with AdminLayout wrapper

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import AdminLayout from '@components/layouts/admin-layout'
+import React from 'react'
+
+export default function LoginPage() {
+    return (
+        <AdminLayout>
+            Heloo
+        </AdminLayout>
+    )
+}
