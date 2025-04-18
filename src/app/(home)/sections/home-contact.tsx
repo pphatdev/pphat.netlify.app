97335f087a5fc57ec66c4dd184129764b3209985
pphatdev <leatsophat02@gmail.com>
2025-04-18 19:30:50 +0700

    Example Get in touch

---
+++ src/app/(home)/sections/home-contact.tsx
@@ -0,0 +1,16 @@
+
+export const HomeContact = () => {
+    return (
+        <section id="section-contact">
+            <div className="max-w-5xl mx-auto p-8 pt-24">
+                <h2 className="mb-4 text-left max-md:text-xl text-5xl font-bold">Get in touch</h2>
+                <p className="mb-10 text-left text-sm text-zinc-500">
+                    {`I'm always open to discussing new projects, creative ideas or opportunities to be part of your vision.`}
+                </p>
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
+                    {/* Add your contact cards here */}
+                </div>
+            </div>
+        </section>
+    )
+}
