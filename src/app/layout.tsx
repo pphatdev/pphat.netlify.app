11ec4f37c7222f6b173fc628ea877a666f747182
pphatdev <leatsophat02@gmail.com>
2025-04-19 08:29:06 +0700

    feat: add Open_Sans font to layout for enhanced typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,18 +4,18 @@ import type { Metadata } from "next";
 import Providers from "../components/ProgressBarProvider";
 import { ThemeProvider } from 'next-themes'
 import { homeHome } from "@lib/meta/home";
-import { aladin, kantumruyPro, poppins, srisakdi } from "@lib/fonts";
+import { aladin, kantumruyPro, poppins, srisakdi, openSans } from "@lib/fonts";
 export { viewport } from "@lib/meta/viewport";
 export const metadata: Metadata = homeHome;
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
     return (
-        <html lang="en" suppressHydrationWarning>
+        <html lang="en" suppressHydrationWarning className="scroll-smooth">
             <head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
             </head>
-            <body className={`${poppins.variable} ${kantumruyPro.variable} ${aladin.variable} ${srisakdi.variable} antialiased p-0 m-0`}>
+            <body className={`${poppins.variable} ${kantumruyPro.variable} ${aladin.variable} ${srisakdi.variable} ${openSans.variable} antialiased p-0 m-0`}>
                 <ThemeProvider
                     attribute="class"
                     enableSystem
