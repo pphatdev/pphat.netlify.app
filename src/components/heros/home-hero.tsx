d416899e92e95a6b489a14e07050f2537a97c5ec
pphatdev <leatsophat02@gmail.com>
2025-04-19 18:15:23 +0700

    Remove unused logo data from demoData in HeroSection component

--- src/components/heros/home-hero.tsx
+++ src/components/heros/home-hero.tsx
@@ -12,58 +12,12 @@ import { NavMenu } from "@components/dock-menu";
 import { ThemeToggle } from "@components/ui/theme-switch";
 import { MagneticArea } from "@components/ui/magnetic-button";
 import { cn } from "@lib/utils";
-import Link from "next/link";
 import { Logos3 } from "@components/ui/logos3";
 import { BlurFade } from "@components/ui/blur-fade";
 import { bgGradientLine45deg } from "@components/background/gradient-line";
-// import { Button } from '../../../components/ui/moving-border';
 
 const demoData = {
     heading: "Trusted by these companies",
-    logos: [
-        {
-            id: "logo-1",
-            description: "Astro",
-            image: "https://www.shadcnblocks.com/images/block/logos/astro.svg",
-            className: "h-7 w-auto",
-        },
-        {
-            id: "logo-2",
-            description: "Figma",
-            image: "https://www.shadcnblocks.com/images/block/logos/figma.svg",
-            className: "h-7 w-auto",
-        },
-        {
-            id: "logo-3",
-            description: "Next.js",
-            image: "https://www.shadcnblocks.com/images/block/logos/nextjs.svg",
-            className: "h-7 w-auto",
-        },
-        {
-            id: "logo-4",
-            description: "React",
-            image: "https://www.shadcnblocks.com/images/block/logos/react.png",
-            className: "h-7 w-auto",
-        },
-        {
-            id: "logo-6",
-            description: "Supabase",
-            image: "https://www.shadcnblocks.com/images/block/logos/supabase.svg",
-            className: "h-7 w-auto",
-        },
-        {
-            id: "logo-7",
-            description: "Tailwind CSS",
-            image: "https://www.shadcnblocks.com/images/block/logos/tailwind.svg",
-            className: "h-4 w-auto",
-        },
-        {
-            id: "logo-8",
-            description: "Vercel",
-            image: "https://www.shadcnblocks.com/images/block/logos/vercel.svg",
-            className: "h-7 w-auto",
-        },
-    ],
 };
 
 
