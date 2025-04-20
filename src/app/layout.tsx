046fb45d19258b2bb463696c22625f79ac4c8b9a
pphatdev <leatsophat02@gmail.com>
2025-04-20 19:14:23 +0700

    Refactor layout component for improved readability and maintainability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,29 @@
-import "./globals.css";
+import "../styles/globals.css";
 import React from "react";
 import type { Metadata } from "next";
 import Providers from "../components/ProgressBarProvider";
 import { ThemeProvider } from 'next-themes'
 import { homeHome } from "@lib/meta/home";
 import { aladin, kantumruyPro, poppins, srisakdi, openSans } from "@lib/fonts";
+import { cn } from "@lib/utils";
 export { viewport } from "@lib/meta/viewport";
 export const metadata: Metadata = homeHome;
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
     return (
-        <html lang="en" suppressHydrationWarning className="scroll-smooth">
+        <html lang="en" suppressHydrationWarning>
             <head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
             </head>
-            <body className={`${poppins.variable} ${kantumruyPro.variable} ${aladin.variable} ${srisakdi.variable} ${openSans.variable} antialiased p-0 m-0`}>
+            <body className={cn(
+                poppins.variable,
+                kantumruyPro.variable,
+                aladin.variable,
+                srisakdi.variable,
+                openSans.variable,
+                `antialiased p-0 m-0 font-sans`
+            )}>
                 <ThemeProvider
                     attribute="class"
                     enableSystem
