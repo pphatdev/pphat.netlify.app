9da40719b7285985ef9ebfd6c542b1365d995ce6
pphatdev <pphat.kits@gmail.com>
2025-04-24 09:56:39 +0700

    Refactor designed export to use IIFE for error handling

--- src/app/(home)/sections/brands.ts
+++ src/app/(home)/sections/brands.ts
@@ -15,12 +15,19 @@ export const languages: Image[] = fs.readdirSync(path.join(publicDir, 'assets/br
         caption: file.split('.')[0].replace(/-/g, ' ').replace(/\b\w/g, char => char.toUpperCase())
     }));
 
-export const designed: Image[] = fs.readdirSync(path.join(publicDir, 'assets/brands/design'))
-    .filter(file => /\.(jpg|jpeg|svg)$/i.test(file))
-    .map(file => ({
-        src: `/assets/brands/design/${file}`,
-        alt: file.split('.')[0],
-        width: 200,
-        height: 200,
-        caption: file.split('.')[0].replace(/-/g, ' ').replace(/\b\w/g, char => char.toUpperCase())
-    }));
+export const designed: Image[] = (() => {
+    try {
+        return fs.readdirSync(path.join(publicDir, 'assets/brands/design'))
+            .filter(file => /\.(jpg|jpeg|svg)$/i.test(file))
+            .map(file => ({
+                src: `/assets/brands/design/${file}`,
+                alt: file.split('.')[0],
+                width: 200,
+                height: 200,
+                caption: file.split('.')[0].replace(/-/g, ' ').replace(/\b\w/g, char => char.toUpperCase())
+            }));
+    } catch (error) {
+        console.error('Error reading directory:', error);
+        return [];
+    }
+})()
