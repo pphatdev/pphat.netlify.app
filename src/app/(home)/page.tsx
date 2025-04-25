aabe7609ff4171cee670daa6d11fc9cea8cc2357
pphatdev <leatsophat02@gmail.com>
2025-04-25 20:33:33 +0700

    refactor: remove unused HomeProjects and HomeTiming components from Home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,10 +4,8 @@ import dynamic from 'next/dynamic';
 import GetInTouchSections from "./sections/home-getintouch";
 import { Metadata } from "next";
 import { appDescriptions, appName, currentDomain } from "@lib/data";
-import { HomeProjects } from './sections/home-project';
 import { BlurFade } from "@components/ui/blur-fade";
 import { HomeSkills } from "./sections/home-skills";
-import { HomeTiming } from "./sections/home-timing";
 
 const NavigationBar = dynamic(() => import('@components/navbar/navbar').then(mod => mod.NavigationBar), {
     ssr: true
@@ -51,19 +49,6 @@ export default function Home() {
             <HeroSection />
             <BlurFade delay={0.25} inView>
                 <HomeSkills />
-            </BlurFade>
-            <BlurFade delay={0.25} inView>
-                <HomeProjects />
-            </BlurFade>
-            <BlurFade delay={0.25} inView>
-                <div
-                    className="w-full relative h-96 bg-foreground/5 bg-center flex items-center justify-center"
-                    style={{
-                        // backgroundImage: "url('https://images.unsplash.com/photo-1533158326339-7f3cf2404354?q=80&w=1068&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
-                    }}
-                    >
-                    <HomeTiming showSeconds showTimezone />
-                </div>
             </BlurFade>
             <BlurFade delay={0.25} inView>
                 <GetInTouchSections />
