0298f6123c3e313d944913a8ee8ae6c1d6f01139
pphatdev <leatsophat02@gmail.com>
2025-04-25 21:12:32 +0700

    feat: migrate portfolio data to whoiam.json and simplify data retrieval in data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,14 +1,7 @@
 import { config } from "dotenv"
 config()
 
-import {
-    name,
-    description,
-    domain,
-    positions
-} from "../../whoiam.data.json"
-
-export const appName = name || "LEAT Sophat"
-export const appPositions = positions || ["Senior Front-end Developer.", "UI/UX Designer."]
-export const appDescriptions =  description || "Hello! I’m Sophat LEAT, also known as PPhat, and I’m thrilled to have you here.\nThis portfolio showcases my journey, projects, and passions as a developer and creator. Explore my work, check out my skills, and feel free to connect if you’d like to collaborate or learn more.\nLet’s build something amazing together!"
-export const currentDomain = process.env.NEXT_PUBLIC_APP_URL?.trim() || domain || "https://pphat.netlify.app"
+export const appName = "LEAT Sophat"
+export const appPositions = ["Senior Front-end Developer.", "UI/UX Designer."]
+export const appDescriptions = "Hello! I’m Sophat LEAT, also known as PPhat, and I’m thrilled to have you here.\nThis portfolio showcases my journey, projects, and passions as a developer and creator. Explore my work, check out my skills, and feel free to connect if you’d like to collaborate or learn more.\nLet’s build something amazing together!"
+export const currentDomain = process.env.NEXT_PUBLIC_APP_URL?.trim() || "https://pphat.netlify.app"
