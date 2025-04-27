724e519b364a0fe8d640f7c8699518996bde709e
pphatdev <leatsophat02@gmail.com>
2025-04-27 19:37:46 +0700

    feat: replace static heading and description with Title component for improved reusability

--- src/app/(home)/sections/home-skills.tsx
+++ src/app/(home)/sections/home-skills.tsx
@@ -1,18 +1,17 @@
-import { GradientLines } from "@components/background/gradient-line"
 import { BlurFade } from "@components/ui/blur-fade"
 import { languages } from "./brands";
 import { MagneticArea } from "@components/ui/magnetic-button";
+import { Title } from "@components/ui/title";
 
 export const HomeSkills = () => {
+
+    const title = ["My ", "Tech Stack"]
+    const description = `My tech stack includes modern frameworks, languages, and development tools that enable me to build efficient and scalable applications.`
+
     return (
-        <section id="section-projects" className="max-w-5xl min-h-[45rem] flex flex-col items-center my-20 justify-center mx-auto">
+        <section id="section-projects" className="max-w-5xl flex flex-col items-center mb-20 justify-center mx-auto">
             <BlurFade delay={0.25} inView className="flex flex-col items-center justify-center pb-1 mt-5">
-                <h2 className="max-md:mb-3 px-5 w-full py-3 max-md:text-3xl text-5xl tracking-tighter font-bold font-sans">
-                    {`My`} <span className="text-left bg-background  bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r from-sky-500 via-teal-500 to-green-500 [text-shadow:0_0_rgba(0,0,0,0.1)]"> Tech stack</span>
-                </h2>
-                <p className="max-md:mb-0 px-5 my-5 text-left text-foreground/500 tracking-normal">
-                    {`My tech stack includes modern frameworks, languages, and development tools that enable me to build efficient and scalable applications.`}
-                </p>
+                <Title as='h2' title={title} description={description} />
                 <div className="w-full flex gap-5 mb-5 rounded-2xl shadow-card shadow-primary/5 bg-background p-8 px-5 flex-wrap justify-start">
                     {languages.map((lang, key) => (
                         <MagneticArea key={key}>
@@ -30,7 +29,6 @@ export const HomeSkills = () => {
                     ))}
                 </div>
             </BlurFade>
-            {/* <GradientLines className="-bottom-1/5 -z-[1]" /> */}
         </section>
     )
 }
