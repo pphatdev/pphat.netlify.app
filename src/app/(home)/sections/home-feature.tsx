a17bda2531ab7b0a2c708e1965bac5245a6e1ca4
pphatdev <leatsophat02@gmail.com>
2025-04-27 19:37:30 +0700

    feat: integrate Title component for enhanced section heading and description

--- src/app/(home)/sections/home-feature.tsx
+++ src/app/(home)/sections/home-feature.tsx
@@ -1,3 +1,4 @@
+import { Title } from "@components/ui/title";
 import { cn } from "@lib/utils";
 import {
     IconCloud,
@@ -29,8 +30,13 @@ export function HomeFeatureSection() {
             icon: <IconRouteAltLeft />,
         },
     ];
+
+    const title = ["What I can", "do for you"];
+    const description = `I can help you build your next project from scratch. I have experience in web development, design, and cloud hosting. I can help you with everything from the initial design to the final deployment.`;
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 relative z-10 pt-10 max-w-5xl mx-auto">
+            <Title as='h2' title={title} description={description} className="col-span-full"/>
             {features.map((feature, index) => (
                 <Feature key={feature.title} {...feature} index={index} />
             ))}
