c5682cf38039aba887ba4d5eb79be8c9c6635d5b
pphatdev <leatsophat02@gmail.com>
2025-04-28 18:31:19 +0700

    refactor: comment out development mode badge in NavbarLogo for cleaner production code

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -240,7 +240,7 @@ export const NavbarLogo = () => {
         <Link href="/" className="shrink-0 items-center flex flex-col z-50" aria-label="Home">
             <Image width={32} height={32} src={'/assets/logo/logo-transparent-dark-mode.png'} alt={"Logo"} className="hidden dark:block" />
             <Image width={32} height={32} src={'/assets/logo/logo-transparent-light-mode.png'} alt={"Logo"} className="dark:hidden" />
-            {process.env.NODE_ENV === "development" && <Badge className="py-0.5 pt-1 h-fit -translate-y-3 bg-background text-[8px] uppercase" variant={"outline"}>Dev Mode</Badge>}
+            {/* {process.env?.NODE_ENV === "development" && <Badge className="py-0.5 pt-1 h-fit -translate-y-3 bg-background text-[8px] uppercase" variant={"outline"}>Dev Mode</Badge>} */}
         </Link>
     );
 };
