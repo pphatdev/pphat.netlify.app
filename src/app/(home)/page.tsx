f2e61275caf2ec025b1bba6d8c9978fa12a08e49
pphatdev <pphat.kits@gmail.com>
2025-04-28 15:08:55 +0700

    feat: move NavigationBar outside of HeroSection for improved layout structure

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -48,8 +48,8 @@ export default function Home() {
     return (
         <div className="w-full relative mx-auto">
             <OrganizationStructuredData />
+            <NavigationBar />
             <section id="hero">
-                <NavigationBar />
                 <HeroSection />
             </section>
 
