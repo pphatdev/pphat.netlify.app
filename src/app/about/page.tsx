54b27582f8f2e53c6c75e262a8ef16b3ef005570
pphatdev <leatsophat02@gmail.com>
2025-05-17 20:28:45 +0700

    feat: add AboutTimeline section with fade-in effect to About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,22 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { NavigationBar } from "@components/navbar/navbar";
 import { AboutMeHero } from "@components/heros/about-hero";
+import { AboutTimeline } from "./sections/timeline";
+import { BlurFade } from '@components/ui/blur-fade';
 
 const Posts = () => {
     return (
         <main className="w-full flex flex-col gap-7 pb-5">
             <NavigationBar />
             <AboutMeHero />
+
+            <section>
+                <BlurFade delay={1}>
+                    <AboutTimeline />
+                </BlurFade>
+            </section>
         </main>
     )
 };
