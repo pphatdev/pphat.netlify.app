e8dfe203420b39c83b5beccc8aeb750ff731bc07
pphatdev <leatsophat02@gmail.com>
2025-05-17 13:56:43 +0700

    fix: remove min-height from HeroSection and update headings for consistency

--- src/components/heros/posts-hero.tsx
+++ src/components/heros/posts-hero.tsx
@@ -21,7 +21,7 @@ export const PostsHero = () => {
             </div>
             <div className="w-full p-4 sm:px-5 flex flex-col max-w-5xl mx-auto ">
                 <BlurFade delay={0.6} inView className="flex flex-col items-start justify-start">
-                    <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"> Po<span className="text-left bg-background  bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r from-sky-500 via-teal-500 to-green-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">sts</span> </h1>
+                    <h1 className="text-3xl font-bold sm:text-5xl xl:text-6xl/none"> Arti<span className="text-left bg-background  bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r from-sky-500 via-teal-500 to-green-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">cles</span> </h1>
                 </BlurFade>
                 <BlurFade delay={0.5} inView className="flex max-sm:order-first flex-col items-start justify-start">
                     <Link href="/" className="hover:bg-foreground/5 font-sans transition-all duration-300 max-sm:mt-0 mt-7 items-center hover:ring w-fit ring-foreground/10 justify-start flex rounded-full hover:px-4 p-1.5">
