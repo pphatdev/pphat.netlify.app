26f969929b457add4afe5098abfcb0c4abd7b444
pphatdev <leatsophat02@gmail.com>
2025-05-17 22:36:47 +0700

    feat: add ProjectsStructuredData component for structured data integration

---
+++ src/components/projects-structured-data.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import React from 'react';
+import { appName, currentDomain } from '@lib/data';
+
+export default function ProjectsStructuredData() {
+    const structuredData = {
+        "@context": "https://schema.org",
+        "@type": "CollectionPage",
+        "mainEntity": {
+            "@type": "ItemList",
+            "itemListElement": [
+                {
+                    "@type": "ListItem",
+                    "position": 1,
+                    "url": `${currentDomain}/projects`,
+                    "name": `${appName} Projects Portfolio`,
+                    "description": "A collection of web development projects created by Leat Sophat (PPhat)"
+                }
+            ]
+        },
+        "name": `${appName} Projects`,
+        "description": "Explore my portfolio of web development projects and applications.",
+        "url": `${currentDomain}/projects`
+    };
+
+    return (
+        <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+        />
+    );
+}
