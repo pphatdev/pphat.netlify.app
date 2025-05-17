db37386897e25397eb691106ccd8ba911bd1c2d4
pphatdev <leatsophat02@gmail.com>
2025-05-17 13:56:26 +0700

    feat: add share button to PostCard component for enhanced sharing functionality

--- src/components/cards/post-card.tsx
+++ src/components/cards/post-card.tsx
@@ -18,27 +18,38 @@ export const PostCard = ({ post }: { post: Post }) => {
     };
 
     return (
-        <div className="relative duration-300 max-sm:flex-col flex gap-0 hover:translate-y-1 overflow-hidden bg-foreground/5 group font-sans rounded-3xl mb-4 ring-foreground/10 hover:ring-primary hover:ring-2 transition-all ease-in-out h-full" role="article" tabIndex={-1}>
+        <div className="relative duration-300 group flex flex-col gap-0 hover:translate-y-1 overflow-hidden bg-foreground/5 group font-sans rounded-3xl mb-4 ring-foreground/10 hover:ring-primary hover:ring-2 transition-all ease-in-out h-full" role="article" tabIndex={-1}>
 
-            <div className="h-full py-4 max-sm:p-4 pl-4 sm:shrink-0">
-                <Image
-                    src={post.thumbnail}
-                    width={200}
-                    height={200}
-                    alt={post.title}
-                    className="w-full max-sm:h-40 h-32 aspect-video object-cover rounded-xl"
-                />
+            <Image
+                src={post.thumbnail}
+                width={200}
+                height={200}
+                alt={post.title}
+                className="w-full h-40 aspect-video object-cover rounded-xl"
+            />
+
+            <div className="absolute opacity-0 bg-background/50 transition-colors group-hover:opacity-100 pointer-events-auto rounded-full right-3 top-3 flex z-50">
+                <div className='bg-foreground/5 z-50 ring-1 w-fit ml-auto ring-foreground/10 justify-end flex rounded-full p-1'>
+                    <Link href={''} className="flex cursor-pointer rounded-full p-2 hover:ring hover:text-background ring-foreground/20 outline-none hover:bg-foreground/10 transition-all items-center justify-center">
+                        <TagIcon className="size-4" />
+                    </Link>
+                    <button
+                        aria-label={`Share ${post.title}`}
+                        type="button"
+                        onClick={sharePost}
+                        className="flex cursor-pointer rounded-full p-2 hover:ring hover:text-background ring-foreground/20 outline-none hover:bg-foreground/10 transition-all items-center justify-center"
+                    >
+                        <Share2Icon className="size-4" aria-hidden="true" />
+                        <span className="sr-only">Share this post</span>
+                    </button>
+                </div>
             </div>
 
             <Link href={post.slug ?? '#'} className="inset-0 z-0 absolute" aria-label={post.title} />
 
-            <div className="p-4 w-full flex flex-col relative pointer-events-none">
+            <div className="px-4 pb-4 pt-2 w-full flex flex-col relative pointer-events-none">
 
-                <div className="flex items-center gap-2 mb-2">
-                    <time dateTime={new Date(post.createdAt).toISOString()} className="text-xs text-foreground/50 font-sans">{new Date(post.createdAt).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })}</time>
-                </div>
-
-                <div className="flex z-50 order-last justify-start items-center flex-wrap gap-2 pointer-events-auto mt-2">
+                <div className="flex z-50 justify-start items-center flex-wrap gap-2 pointer-events-auto mt-2">
                     {post.tags.slice(0, 3).map((tag, index) => (
                         <Link key={index} href={`?tag=${tag}`} className="text-xs font-sans">
                             <Badge variant="outline" className="bg-foreground/5 text-foreground/80 hover:bg-foreground/10 hover:text-primary transition-all duration-200 ease-in-out">{tag}</Badge>
@@ -46,25 +57,12 @@ export const PostCard = ({ post }: { post: Post }) => {
                     ))}
                 </div>
 
+                <div className="flex items-center gap-2 my-2">
+                    <time dateTime={new Date(post.createdAt).toISOString()} className="text-xs text-foreground/50 font-sans">{new Date(post.createdAt).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })}</time>
+                </div>
+
                 <h2 className="z-10 font-semibold font-sans tracking-wide line-clamp-1 pb-1">{post.title}</h2>
                 <p className='font-normal text-sm z-10 line-clamp-4 text-foreground/80'>{post.content}</p>
-
-                <div className="absolute pointer-events-auto rounded-full right-3 top-3 flex z-50">
-                    <div className='bg-foreground/5 z-50 ring-1 w-fit ml-auto ring-foreground/10 justify-end flex rounded-full p-1'>
-                        <Link href={''} className="flex cursor-pointer rounded-full p-2 hover:ring hover:text-primary ring-foreground/20 outline-none hover:bg-foreground/10 transition-all items-center justify-center">
-                            <TagIcon className="size-4" />
-                        </Link>
-                        <button
-                            aria-label={`Share ${post.title}`}
-                            type="button"
-                            onClick={sharePost}
-                            className="flex cursor-pointer rounded-full p-2 hover:ring hover:text-primary ring-foreground/20 outline-none hover:bg-foreground/10 transition-all items-center justify-center"
-                        >
-                            <Share2Icon className="size-4" aria-hidden="true" />
-                            <span className="sr-only">Share this post</span>
-                        </button>
-                    </div>
-                </div>
             </div>
         </div>
     );
